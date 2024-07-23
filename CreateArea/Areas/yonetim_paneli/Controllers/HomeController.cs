0bd410114f77caf0185a46602df88a1f5e74b97a
Erdoğan Mücahit Alımcı <mcht.almc@gmail.com>
2024-07-23 11:16:27 +0300

    Add project files.

---
+++ CreateArea/Areas/yonetim_paneli/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CreateArea.Areas.yonetim_paneli.Controllers
+{
+	[Area("yonetim_paneli")]   // => Area attribute kullanarak hangi controller'in hangi area'da oldugunu bildirmem gerekmektedir.
+	//Bulundugu area kulasoru icerisinde iss eo ismi vermek kavul edilen bir davranistir.
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
